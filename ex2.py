d82de2157f3b7adab479cf366013952ff471d37b
owalton <ftbqoc@gmail.com>
2017-05-03 10:19:23 -0600

    updates

---
+++ ex2.py
@@ -0,0 +1,10 @@
+# A comment, this is so you can read your program later 
+# Anything after the # is ignored by python.
+
+print "I could have cde like this." # and the comment after is ignored 
+
+# You can also use comment to "disable" or comment out a pice of code:
+# pring "This won't run."
+
+print "This will run."
+
---
+++ ex3.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+#Author: Octavius Walton
+#Date: 
+#Purpose Exercise 3
+
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30 / 6					# total of Hens
+print "Roosters", 100 - 25 * 3 % 4			# total of Roosters
+
+print "Now I will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6     # Counting eggs 
+
+print "Is it true that 3 + 2 < 5 - 7?"		# Asking a true or false question 
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+
+print "What is  5 - 7", 5 - 7
+
+print "Oh, that's why it's False"
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2				# true or false answer to the question
+print "Is it greater or equal?", 5 >= -2	# true or false answer to the question
+print "Is it less or equal?", 5 <= -2		# true or false answer to the question
